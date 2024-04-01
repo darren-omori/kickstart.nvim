8e0cb2efa69b7fc9d6f5c27741a905fcd11d50b2
Darren Omori <darren.omori0@gmail.com>
2024-04-01 13:28:41 -0700

    [feat] add pylsp config

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -541,6 +541,34 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                ruff = {
+                  enabled = true,
+                  select = { 'ALL' },
+                  -- Iteratively add to this list to ignore rules
+                  ignore = { 'D', 'ANN101', 'ANN204', 'FA' },
+                  lineLength = 100,
+                },
+                mypy = {
+                  enabled = true,
+                },
+                pylint = {
+                  enabled = true,
+                  -- only enabling import resolution rules as ruff doesn't support this currently.
+                  -- TODO: need to think of a better solution long term for the hook.
+                  args = {
+                    '--disable=all',
+                    '--enable=E0401,E0611',
+                    '--init-hook=\'import sys; sys.path.append("/Users/doomori/workplace/naia/venv/lib/python3.8/site-packages")\'',
+                  },
+                },
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --

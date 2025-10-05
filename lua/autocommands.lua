fffe32f7da6f81195ccea71ef22199d6ec0f2ede
Darren Omori <doomori@amazon.com>
2025-10-05 11:39:34 +0000

    feat: yank over SSH

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -10,3 +10,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+--  Allow yanking to write to system keyboard over SSH
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    -- vim.highlight.on_yank()
+    local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
+    copy_to_unnamedplus(vim.v.event.regcontents)
+    local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
+    copy_to_unnamed(vim.v.event.regcontents)
+  end,
+})
